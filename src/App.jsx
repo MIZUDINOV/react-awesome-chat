7f3863bed116a8ef8fdcf178ee3efe8355370c4e
Muhammad <mui.xeon@gmail.com>
2021-06-26 22:32:49 +0300

    finish Authentication component styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Authentication } from './Authentication'
 
 function App() {
-  return <div className='app'></div>
+  return (
+    <div
+      style={{
+        maxWidth: '760px',
+        margin: '0 auto',
+        height: '100vh',
+        display: 'flex',
+      }}
+    >
+      <Authentication />
+    </div>
+  )
 }
 
 export default App
--- src/Authentication.jsx
+++ src/Authentication.jsx
@@ -1,5 +1,28 @@
+import { Button, createStyles, Typography } from '@material-ui/core'
 import React from 'react'
 
 export const Authentication = () => {
-  return <div></div>
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '300px',
+      }}
+    >
+      <Typography
+        style={{ marginBottom: '25px' }}
+        variant='h5'
+        component='h3'
+        align='center'
+      >
+        Вы можете войти в чат используя анонимный аккаунт
+      </Typography>
+      <Button color='primary' variant='contained' align='center'>
+        Войти
+      </Button>
+    </div>
+  )
 }
